19fc8f8c75c5b35bead67cce530f24993184b082
lee10104 <lee95625@gmail.com>
2017-12-17 16:31:29 +0900

    프로젝트2 시작

---
+++ DBProject2/src/ConcertManager.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConcertManager {
+    String serverName = "147.46.15.147";
+    String dbName = "db2013-11422";
+    String userName = "u2013-11422";
+    String password = "a5716f255ddf";
+    
+    String url = "jdbc:mariadb://" + serverName + "/" + dbName;
+    
+    public static void main(String args[]) {
+        
+    }
+}
